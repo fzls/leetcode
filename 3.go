8a2849ffff00bcdc08eca31c0d17a009bfbb6503
Chen Ji <fzls.zju@gmail.com>
2019-08-16 01:15:08 +0800

    参考题解，优化算法

--- 3.go
+++ 3.go
@@ -1,24 +1,24 @@
 package leetcode
 
 func lengthOfLongestSubstring(s string) int {
-	var maxLen int
-	// 依次遍历子字符串左侧下标
-	for i := 0; i < len(s) && i+maxLen < len(s); {
-		// 记录从这里开始的已出现的字符，及其位置
-		mapChar2Idx := make(map[byte]int)
-		for j := i; j < len(s); j++ {
-			if idx, exists := mapChar2Idx[s[j]]; !exists {
-				// 若此字符在之前未出现，则尝试更新最大值，并记录位置
-				if j-i+1 > maxLen {
-					maxLen = j - i + 1
-				}
-				mapChar2Idx[s[j]] = j
-			} else {
-				// 否则直接将左侧下标推进至与之前出现的本字符的后面一位
-				i = idx + 1
-				break
+	var maxLen, i, j int
+	// 滑动窗口
+	mapChar2Idx := make(map[byte]int)
+	for i < len(s) && j < len(s) && i+maxLen < len(s) {
+		if idx, exists := mapChar2Idx[s[j]]; !exists {
+			// 若此字符在之前未出现，则尝试更新最大值，并记录位置
+			if j-i+1 > maxLen {
+				maxLen = j - i + 1
 			}
+		} else {
+			for _i := i; _i <= idx; _i++ {
+				delete(mapChar2Idx, s[_i])
+			}
+			// 否则直接将左侧下标推进至与之前出现的本字符的后面一位
+			i = idx + 1
 		}
+		mapChar2Idx[s[j]] = j
+		j++
 	}
 
 	return maxLen
--- 3_test.go
+++ 3_test.go
@@ -15,7 +15,17 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 			name string
 			args args
 			want int
-		}{name: "case", args: struct{ s string }{s: "abcde"}, want: 5},
+		}{name: "case1", args: struct{ s string }{s: "abcabcbb"}, want: 3},
+		struct {
+			name string
+			args args
+			want int
+		}{name: "case2", args: struct{ s string }{s: "bbbbb"}, want: 1},
+		struct {
+			name string
+			args args
+			want int
+		}{name: "case3", args: struct{ s string }{s: "pwwkew"}, want: 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
