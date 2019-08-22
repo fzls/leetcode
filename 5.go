543f8103489010d9b749ec2d9b7b8902b5765251
Chen Ji <fzls.zju@gmail.com>
2019-08-23 01:52:05 +0800

    改用动态规划

--- 5.go
+++ 5.go
@@ -1,7 +1,7 @@
 package leetcode
 
 func longestPalindrome(s string) string {
-	if len(s) == 0 {
+	if len(s) <= 1 {
 		return s
 	}
 
@@ -9,33 +9,43 @@ func longestPalindrome(s string) string {
 		maxI = 0
 		maxJ = 0
 	)
-	for i:=0; i < len(s); i++{
-		for j:=i+1; j<len(s); j++{
-			if j-i > maxJ-maxI && isPalindrome(s[i:j+1]) {
+	var isPalindrome [1000][1000]bool
+	// f(i,i)
+	for i := 0; i < len(s); i++ {
+		isPalindrome[i][i] = true
+	}
+	// f(i,i+1)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			isPalindrome[i][i+1] = true
+			if maxJ-maxI < 1 {
 				maxI = i
-				maxJ = j
+				maxJ = i + 1
 			}
+		} else {
+			isPalindrome[i][i+1] = false
 		}
 	}
 
-	return s[maxI:maxJ+1]
-}
+	// f(i,j) = false if s[i]!=s[j], f(i+1,j-1) if s[i] == s[j], for any 0<=i<n, i+2<=j<n
+	for step := 2; step < len(s); step++ {
+		i := 0
+		j := i + step
+		for j < len(s) {
+			if s[i] == s[j] {
+				isPalindrome[i][j] = isPalindrome[i+1][j-1]
+				if maxJ-maxI < step && isPalindrome[i][j] {
+					maxI = i
+					maxJ = j
+				}
+			} else {
+				isPalindrome[i][j] = false
+			}
 
-func isPalindrome(s string) bool {
-	if len(s) <= 1{
-		return true
-	}
-
-	i:=0
-	j:=len(s) - 1
-	for i<j {
-		if s[i]!=s[j]{
-			return false
+			i++
+			j++
 		}
-
-		i++
-		j--
 	}
 
-	return true
+	return s[maxI : maxJ+1]
 }
