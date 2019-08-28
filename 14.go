c94b2943fa759c1f882ea25357e89ee7c93dab5d
Chen Ji <fzls.zju@gmail.com>
2019-08-29 00:42:31 +0800

    添加备注题解中的其他解法

--- 14.go
+++ 14.go
@@ -1,5 +1,11 @@
 package leetcode
 
+// 其他解法：
+// 分治法：LCP(s[i,j]) = LCP(LCP(s[i, m]), LCP(s[m+1, j]))
+// 前缀树：Trie：根节点为无意义的一个节点
+// 		构造：遍历每个字符串，当当前字符不在当前节点的子节点中，则新增一个该字符的子节点，否则设置当前节点为该子节点
+// 		查询最大公共串：从根节点往下遍历，若子节点数目为1，则添加该节点的值到结果中，若不为1则返回当前结果
+
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
