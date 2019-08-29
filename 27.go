d1e14e06ea41dd33a4b46a3660329fff0104d44d
Chen Ji <fzls.zju@gmail.com>
2019-08-30 01:04:05 +0800

    尝试官方题解中的第二种解法

--- 27.go
+++ 27.go
@@ -5,14 +5,24 @@ func removeElement(nums []int, val int) int {
 		return 0
 	}
 
-	i := 0
-	for j := 0; j < len(nums); j++ {
-		if nums[j] != val {
-			// 将不等于val的值依次放到前方
-			nums[i] = nums[j]
-			i++
+	//i := 0
+	//for j := 0; j < len(nums); j++ {
+	//	if nums[j] != val {
+	//		// 将不等于val的值依次放到前方
+	//		nums[i] = nums[j]
+	//		i++
+	//	}
+	//}
+
+	i := len(nums) - 1
+	for j := 0; j <= i; j++ {
+		for i >= 0 && j <= i && nums[j] == val {
+			// 将等于val的值依次放到最后
+			nums[j] = nums[i]
+			nums[i] = val
+			i--
 		}
 	}
 
-	return i
+	return i + 1
 }
