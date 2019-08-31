dc2096435d50f8dd39ec80123de56e75257630c0
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 03:13:16 +0800

    88

---
+++ 88_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	t.Run("test", func(t *testing.T) {
+		merge(nums1, 3, nums2, 3)
+		if !reflect.DeepEqual(nums1, []int{1, 2, 2, 3, 5, 6}) {
+			t.Errorf("merge() = %v, want %v", nums1, []int{1, 2, 2, 3, 5, 6})
+		}
+	})
+}
