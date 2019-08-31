c0e8c112265a1bc8a224d90238cad10443846bc2
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 03:26:04 +0800

    将公共结构体移到单独的文件中

--- 2.go
+++ 2.go
@@ -1,10 +1,11 @@
 package leetcode
 
-//Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+//
+////Definition for singly-linked list.
+//type ListNode struct {
+//	Val  int
+//	Next *ListNode
+//}
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummyHead := &ListNode{}
