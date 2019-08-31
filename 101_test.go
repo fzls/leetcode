8f53ce6678bc4336ab00a0bdaea7e3f5463c29f6
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 03:41:59 +0800

    101

---
+++ 101_test.go
@@ -0,0 +1,72 @@
+package leetcode
+
+import "testing"
+
+func Test_isSymmetric(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test", args: struct{ root *TreeNode }{root: &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   4,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   4,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		}}, want: true},
+		{name: "test", args: struct{ root *TreeNode }{root: &TreeNode{
+			Val: 1,
+			Left: &TreeNode{
+				Val:  2,
+				Left: nil,
+				Right: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Val:  2,
+				Left: nil,
+				Right: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		}}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSymmetric(tt.args.root); got != tt.want {
+				t.Errorf("isSymmetric() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
