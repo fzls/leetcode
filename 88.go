cda2e1c74bbb5cd56eaf31a17eff21ecd9f7a7e2
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 03:23:22 +0800

    88 参考题解，无需额外移位

--- 88.go
+++ 88.go
@@ -1,36 +1,27 @@
 package leetcode
 
-func getInsertPosition(nums1 []int, currentLen int, val int) int {
-	low := 0
-	high := currentLen - 1
-	for low <= high {
-		j := low + (high-low)>>1
-		if nums1[j] <= val {
-			low = j + 1
-		} else {
-			high = j - 1
-		}
-	}
-
-	return low
-}
-
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	if n == 0 {
 		return
 	}
 
-	right := m
-	for i := 0; i < n; i++ {
-		// 找到插入的位置
-		ip := getInsertPosition(nums1, right, nums2[i])
-
-		// 插入
-		for idx := right; idx > ip; idx-- {
-			nums1[idx] = nums1[idx-1]
+	idx := m + n - 1
+	i := m - 1
+	j := n - 1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[idx] = nums1[i]
+			i--
+		} else {
+			nums1[idx] = nums2[j]
+			j--
 		}
-		nums1[ip] = nums2[i]
 
-		right++
+		idx--
+	}
+	for j >= 0 {
+		nums1[idx] = nums2[j]
+		j--
+		idx--
 	}
 }
--- 88_test.go
+++ 88_test.go
@@ -6,12 +6,24 @@ import (
 )
 
 func Test_merge(t *testing.T) {
-	nums1 := []int{1, 2, 3, 0, 0, 0}
-	nums2 := []int{2, 5, 6}
-	t.Run("test", func(t *testing.T) {
-		merge(nums1, 3, nums2, 3)
-		if !reflect.DeepEqual(nums1, []int{1, 2, 2, 3, 5, 6}) {
-			t.Errorf("merge() = %v, want %v", nums1, []int{1, 2, 2, 3, 5, 6})
-		}
-	})
+	{
+		nums1 := []int{1, 2, 3, 0, 0, 0}
+		nums2 := []int{2, 5, 6}
+		t.Run("test", func(t *testing.T) {
+			merge(nums1, 3, nums2, 3)
+			if !reflect.DeepEqual(nums1, []int{1, 2, 2, 3, 5, 6}) {
+				t.Errorf("merge() = %v, want %v", nums1, []int{1, 2, 2, 3, 5, 6})
+			}
+		})
+	}
+	{
+		nums1 := []int{4, 5, 6, 0, 0, 0}
+		nums2 := []int{1, 2, 3}
+		t.Run("test", func(t *testing.T) {
+			merge(nums1, 3, nums2, 3)
+			if !reflect.DeepEqual(nums1, []int{1, 2, 3, 4, 5, 6}) {
+				t.Errorf("merge() = %v, want %v", nums1, []int{1, 2, 3, 4, 5, 6})
+			}
+		})
+	}
 }
