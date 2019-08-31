a4694dc1c0066acabd1af83b1a9325aa4a5fd105
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 00:56:03 +0800

    53

---
+++ 53.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	maxSum := nums[0]
+	currentSum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if currentSum < 0 {
+			currentSum = 0
+		}
+
+		currentSum += nums[i]
+
+		if currentSum > maxSum {
+			maxSum = currentSum
+		}
+	}
+
+	return maxSum
+}
