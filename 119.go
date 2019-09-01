6f52dd2b3510c77676260bbebb3a7f670c8c9e2d
风之凌殇 <fzls.zju@gmail.com>
2019-09-01 21:42:31 +0800

    119

---
+++ 119.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func getRow(rowIndex int) []int {
+	var res [][]int
+
+	for i := 0; i <= rowIndex; i++ {
+		row := make([]int, i+1)
+
+		row[0] = 1
+		row[i] = 1
+
+		for j := 1; j < i; j++ {
+			// j>=1, i>=2
+			row[j] = res[i-1][j-1] + res[i-1][j]
+		}
+
+		res = append(res, row)
+	}
+
+	return res[rowIndex]
+}
