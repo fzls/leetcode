bacd91cf7674958d0dd8f472a255108be179a8a5
Chen Ji <fzls.zju@gmail.com>
2019-09-04 04:16:03 +0800

    189 使用之前从陈越姥姥那边学到的环法

--- 189.go
+++ 189.go
@@ -5,23 +5,18 @@ func rotate(nums []int, k int) {
 		return
 	}
 
-	// 使用编程之美中的逆转法
-	// 1~n-k n-k+1~n
-	// n-k~1 n~n-k+1
-	// n-k+1~n 1~n-k
+	// 使用之前从陈越姥姥那边学到的环法
 	k = k % len(nums)
-	_reverse(nums, 0, len(nums)-k-1)
-	_reverse(nums, len(nums)-k, len(nums)-1)
-	_reverse(nums, 0, len(nums)-1)
-}
-
-func _reverse(nums []int, b int, e int) {
-	for b < e {
-		temp := nums[b]
-		nums[b] = nums[e]
-		nums[e] = temp
-
-		b++
-		e--
+	cnt := 0
+	for i := 0; i < len(nums) && cnt < len(nums); i++ {
+		buf := nums[i]
+		current := i
+		for j := (i + len(nums) - k) % len(nums); j != i; j = (j + len(nums) - k) % len(nums) {
+			nums[current] = nums[j]
+			current = j
+			cnt++
+		}
+		nums[current] = buf
+		cnt++
 	}
 }
