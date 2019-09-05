03c4cf5898635dc688685d70e26ae693fe34442b
风之凌殇 <fzls.zju@gmail.com>
2019-09-06 04:16:24 +0800

    206 迭代解法

--- 206.go
+++ 206.go
@@ -7,29 +7,27 @@ package leetcode
  *     Next *ListNode
  * }
  */
-func recurReverseList(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-
-	_head := head
-	if head.Next != nil {
-		// 循环将后续列表翻转
-		_head = recurReverseList(head.Next)
-		// 将下一个节点的Next指向本节点
-		head.Next.Next = head
-	}
-	return _head
-}
 
 func reverseList(head *ListNode) *ListNode {
 	if head == nil {
 		return nil
 	}
 
-	// 循环将后续列表翻转
-	res := recurReverseList(head)
-	// 将原先的头结点的Next指向nil
+	cur := head
+	next := cur.Next
+	for next != nil {
+		// 保存下下个节点
+		nextNext := next.Next
+
+		// 将下个节点指向本节点
+		next.Next = cur
+
+		// 迭代
+		cur = next
+		next = nextNext
+	}
+	// 将原头结点的Next指向nil
 	head.Next = nil
-	return res
+
+	return cur
 }
