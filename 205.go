7b9dad215b1675efd9987f37df41d309ff34d688
风之凌殇 <fzls.zju@gmail.com>
2019-09-06 03:55:14 +0800

    205 添加注释

--- 205.go
+++ 205.go
@@ -1,7 +1,6 @@
 package leetcode
 
 func getIndexes(s string) []int {
-
 	firstAppearIndex := make([]int, 256)
 	idx := 1
 	var indexes []int
@@ -23,6 +22,9 @@ func isIsomorphic(s string, t string) bool {
 		return true
 	}
 
+	// 假设某个字符char在字符串中首次出现的下标为idx[char]
+	// 将字符串每个字母替换为其首次出现的下标，则构成一个下标数组
+	// 两个字符串同构等价于下标数组一致
 	indexes1 := getIndexes(s)
 	indexes2 := getIndexes(t)
 	for i := 0; i < len(s); i++ {
