84eef98e7c80c6148dc9eda343dd4e89778de9dc
风之凌殇 <fzls.zju@gmail.com>
2019-09-06 03:27:31 +0800

    203

---
+++ 203.go
@@ -0,0 +1,25 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	dummyHead := &ListNode{
+		Next: head,
+	}
+
+	p := dummyHead
+	for p.Next != nil {
+		if p.Next.Val == val {
+			p.Next = p.Next.Next
+		} else {
+			p = p.Next
+		}
+	}
+
+	return dummyHead.Next
+}
