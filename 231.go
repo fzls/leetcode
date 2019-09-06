bea460feaa39b03c24d34bf3084372e7d01844f5
Chen Ji <fzls.zju@gmail.com>
2019-09-07 00:28:38 +0800

    231 参考题解，使用更简洁的表示

--- 231.go
+++ 231.go
@@ -2,14 +2,5 @@ package leetcode
 
 func isPowerOfTwo(n int) bool {
 	//等价于判断n的二进制表示中仅有1个1
-	cntOfOne := 0
-	for n != 0 {
-		cntOfOne++
-		n = n & (n - 1)
-
-		if cntOfOne > 1 {
-			return false
-		}
-	}
-	return cntOfOne == 1
+	return n > 0 && n&(n-1) == 0
 }
