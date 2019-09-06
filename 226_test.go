fa1cf12657b6068cbe13884016968561319491d0
Chen Ji <fzls.zju@gmail.com>
2019-09-07 00:20:13 +0800

    226

---
+++ 226_test.go
@@ -0,0 +1,86 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_invertTree(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		struct {
+			name string
+			args args
+			want *TreeNode
+		}{name: "test", args: struct{ root *TreeNode }{root: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   1,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Val: 7,
+				Left: &TreeNode{
+					Val:   6,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   9,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		}}, want: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val: 7,
+				Left: &TreeNode{
+					Val:   9,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   6,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+			Right: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val:   3,
+					Left:  nil,
+					Right: nil,
+				},
+				Right: &TreeNode{
+					Val:   1,
+					Left:  nil,
+					Right: nil,
+				},
+			},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := invertTree(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("invertTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
