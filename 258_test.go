a8a8e0b771dbd70fbdfc5dacd111a97bc63e1f04
风之凌殇 <fzls.zju@gmail.com>
2019-09-08 02:15:58 +0800

    258

---
+++ 258_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_addDigits(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		struct {
+			name string
+			args args
+			want int
+		}{name: "test", args: struct{ num int }{num: 38}, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addDigits(tt.args.num); got != tt.want {
+				t.Errorf("addDigits() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
