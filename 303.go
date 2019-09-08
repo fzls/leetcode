13095e31c8c3ca7d022f788f8f7bf4940c66045d
风之凌殇 <fzls.zju@gmail.com>
2019-09-09 01:56:19 +0800

    303 参考题解，优化存储和计算次数

--- 303.go
+++ 303.go
@@ -1,1 +1,36 @@
 package leetcode
+
+type NumArray struct {
+	nums []int
+
+	kSums []int
+}
+
+func Constructor___(nums []int) NumArray {
+	na := NumArray{
+		nums: nums,
+	}
+
+	na.preCompute()
+
+	return na
+}
+
+func (this *NumArray) preCompute() {
+	sum := 0
+	for i := 0; i < len(this.nums); i++ {
+		sum += this.nums[i]
+
+		this.kSums = append(this.kSums, sum)
+	}
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	return this.kSums[j] - this.kSums[i] + this.nums[i]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */
