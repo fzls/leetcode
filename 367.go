6d1d23fb08e14df325e6538d4475be7bda9829ed
Chen Ji <fzls.zju@gmail.com>
2019-09-09 06:36:32 +0800

    367 使用二分查找

--- 367.go
+++ 367.go
@@ -5,9 +5,17 @@ func isPerfectSquare(num int) bool {
 		return true
 	}
 
-	for i := 2; i <= num/2; i++ {
-		if i*i == num {
+	low := 2
+	high := num / 2
+	for low <= high {
+		i := low + (high-low)/2
+		pow2 := i * i
+		if pow2 == num {
 			return true
+		} else if pow2 < num {
+			low = i + 1
+		} else {
+			high = i - 1
 		}
 	}
 
