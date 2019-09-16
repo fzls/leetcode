3cde5564820415439547957bcdf6166c68445f68
风之凌殇 <fzls.zju@gmail.com>
2019-09-17 00:50:33 +0800

    11 参考题解进行优化

--- 11.go
+++ 11.go
@@ -4,13 +4,20 @@ package leetcode
 func maxArea(height []int) int {
 	max := 0
 
-	// 暴力解法
-	for i := 0; i < len(height); i++ {
-		for j := i + 1; j < len(height); j++ {
-			a := area(height, i, j)
-			if a > max {
-				max = a
-			}
+	// 参考题解，使用双指针分别指向两侧
+	// 若移动较大者向内，因为长度变短了，而新的高度不会高于原来的较小者，所以面积必定下降
+	// 而移动较小者向内，则有可能会使面积向下，所以每次决策均移动较小者向内
+	i := 0
+	j := len(height) - 1
+	for i < j {
+		a := area(height, i, j)
+		if a > max {
+			max = a
+		}
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
 		}
 	}
 
