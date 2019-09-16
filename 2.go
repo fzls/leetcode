38382cddc59260274b9ed618a7648aaa1d754ae0
风之凌殇 <fzls.zju@gmail.com>
2019-09-17 01:16:07 +0800

    2 整理代码，使其更整洁

--- 2.go
+++ 2.go
@@ -11,42 +11,32 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	dummyHead := &ListNode{}
 	current := dummyHead
 
-	addon := 0
+	carry := 0
 	// 当两个链表都没有到底的时候，同时推进，并依次相加，计算溢出值
-	for ; l1 != nil && l2 != nil; l1, l2 = l1.Next, l2.Next {
+	for l1 != nil || l2 != nil {
+		sum := carry
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
 		current.Next = &ListNode{}
 		current = current.Next
-
-		sum := l1.Val + l2.Val + addon
 		current.Val = sum % 10
-		addon = sum / 10
-	}
-
-	// 推进还有元素未处理的列表
-	for ; l1 != nil; l1 = l1.Next {
-		current.Next = &ListNode{}
-		current = current.Next
-
-		sum := l1.Val + addon
-		current.Val = sum % 10
-		addon = sum / 10
-	}
-
-	for ; l2 != nil; l2 = l2.Next {
-		current.Next = &ListNode{}
-		current = current.Next
-
-		sum := l2.Val + addon
-		current.Val = sum % 10
-		addon = sum / 10
+		carry = sum / 10
 	}
 
 	// 两条链表均处理完后若仍有溢出，则额外增加一个节点
-	if addon != 0 {
+	if carry != 0 {
 		current.Next = &ListNode{}
 		current = current.Next
 
-		current.Val = addon
+		current.Val = carry
 	}
 
 	return dummyHead.Next
