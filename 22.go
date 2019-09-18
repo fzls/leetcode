abc9b7fd7b7fef570af9d6855a058c3444d2925a
风之凌殇 <fzls.zju@gmail.com>
2019-09-18 23:25:58 +0800

    22 参考官方的dp解法

--- 22.go
+++ 22.go
@@ -1,7 +1,11 @@
 package leetcode
 
+import "fmt"
+
 // 2019/09/18 22:58 by fzls
 func generateParenthesis(n int) []string {
+	return _dp_generateParenthesis(n)
+
 	var res []string
 	_generateParenthesis(0, 2*n, []byte{}, &res)
 	return res
@@ -28,4 +32,24 @@ func _generateParenthesis(nUnpaired int, nRemaining int, temp []byte, res *[]str
 		// 未匹配数不为0， 则说明本次可以尝试放右括号
 		_generateParenthesis(nUnpaired-1, nRemaining-1, append(temp, ')'), res)
 	}
+}
+
+// 参考官方题解的动态规划方法
+// 因为第一个位置必定为左括号，我们可以通过枚举其匹配的右括号的所有可能位置，将问题转化为子问题
+// 假设dp(n)是n个左右括号的所有组合，右括号可能在1,3,5,...2*i+1,...2*(n-1)+1 的位置，假设为位置为2*i+1，
+// 则这种情况下所有可能可以表示为 ( + left + ) + right，其中left=dp(i), right = dp(n-i-1)
+func _dp_generateParenthesis(n int) []string {
+	if n == 0 {
+		return []string{""}
+	}
+
+	var res []string
+	for i := 0; i < n; i++ {
+		for _, left := range _dp_generateParenthesis(i) {
+			for _, right := range _dp_generateParenthesis(n - i - 1) {
+				res = append(res, fmt.Sprintf("(%v)%v", left, right))
+			}
+		}
+	}
+	return res
 }
