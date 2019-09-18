58a86c0afd0d4e610653dd2135e3577fba13988a
风之凌殇 <fzls.zju@gmail.com>
2019-09-18 23:32:54 +0800

    22 实现自底向上的dp

--- 22.go
+++ 22.go
@@ -38,25 +38,23 @@ func _generateParenthesis(nUnpaired int, nRemaining int, temp []byte, res *[]str
 // 因为第一个位置必定为左括号，我们可以通过枚举其匹配的右括号的所有可能位置，将问题转化为子问题
 // 假设dp(n)是n个左右括号的所有组合，右括号可能在1,3,5,...2*i+1,...2*(n-1)+1 的位置，假设为位置为2*i+1，
 // 则这种情况下所有可能可以表示为 ( + left + ) + right，其中left=dp(i), right = dp(n-i-1)
-var cache_dp_generateParenthesis = make(map[int][]string)
-
 func _dp_generateParenthesis(n int) []string {
-	if c, ok := cache_dp_generateParenthesis[n]; ok {
-		return c
-	}
-
 	if n == 0 {
 		return []string{""}
 	}
 
-	var res []string
-	for i := 0; i < n; i++ {
-		for _, left := range _dp_generateParenthesis(i) {
-			for _, right := range _dp_generateParenthesis(n - i - 1) {
-				res = append(res, fmt.Sprintf("(%v)%v", left, right))
+	dp := make([][]string, n+1)
+	dp[0] = []string{""}
+
+	for idx := 1; idx <= n; idx++ {
+		for i := 0; i < idx; i++ {
+			for _, left := range dp[i] {
+				for _, right := range dp[idx-i-1] {
+					dp[idx] = append(dp[idx], fmt.Sprintf("(%v)%v", left, right))
+				}
 			}
 		}
 	}
-	cache_dp_generateParenthesis[n] = res
-	return res
+
+	return dp[n]
 }
