27f5a9368864604b8eda0c090b149fa6a04b5373
风之凌殇 <fzls.zju@gmail.com>
2019-09-20 01:49:51 +0800

    19 看了下文章回顾了下，自己实现了简单的堆

--- 23.go
+++ 23.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "container/heap"
-
 // 2019/09/20 0:47 by fzls
 /**
  * Definition for singly-linked list.
@@ -17,23 +15,62 @@ type ListHeap struct {
 func (lh *ListHeap) Len() int {
 	return len(lh.data)
 }
-
-func (lh *ListHeap) Less(i, j int) bool {
-	return lh.data[i].Val < lh.data[j].Val
+func (lh *ListHeap) Push(x *ListNode) {
+	// 放到末尾并上移
+	lh.data = append(lh.data, x)
+	lh.up(len(lh.data) - 1)
 }
 
-func (lh *ListHeap) Swap(i, j int) {
-	lh.data[i], lh.data[j] = lh.data[j], lh.data[i]
-}
+func (lh *ListHeap) Pop() *ListNode {
+	// 将头部取出作为结果，并将原先的最后一个放到该处，然后下移
+	top := lh.data[0]
 
-func (lh *ListHeap) Push(x interface{}) {
-	lh.data = append(lh.data, x.(*ListNode))
-}
-
-func (lh *ListHeap) Pop() interface{} {
-	top := lh.data[len(lh.data)-1]
+	lh.data[0] = lh.data[len(lh.data)-1]
 	lh.data = lh.data[:len(lh.data)-1]
+	lh.down(0)
+
 	return top
+}
+
+func (lh *ListHeap) down(i int) {
+	n := len(lh.data)
+	for i < n {
+		// 找到左右子节点中的较小者
+		// 左节点
+		left := 2*i + 1
+
+		if left >= n {
+			break
+		}
+		minChild := left
+		// 右节点
+		if right := 2*i + 2; right < n && lh.data[right].Val < lh.data[left].Val {
+			minChild = right
+		}
+		// 判断是否比子节点小，若是则无需移动
+		if lh.data[i].Val <= lh.data[minChild].Val {
+			break
+		}
+
+		// 否则交换位置，继续
+		lh.data[i], lh.data[minChild] = lh.data[minChild], lh.data[i]
+		i = minChild
+	}
+}
+
+func (lh *ListHeap) up(i int) {
+	for i != 0 {
+		// 找到父节点
+		parent := (i - 1) / 2
+
+		// 判断是否需要上移
+		if lh.data[i].Val < lh.data[parent].Val {
+			lh.data[i], lh.data[parent] = lh.data[parent], lh.data[i]
+			i = parent
+		} else {
+			break
+		}
+	}
 }
 
 func mergeKLists(lists []*ListNode) *ListNode {
@@ -41,22 +78,21 @@ func mergeKLists(lists []*ListNode) *ListNode {
 
 	// heap的写法记不太清了，先用官方提供的最小堆实现吧- -
 	listHeap := &ListHeap{}
-	heap.Init(listHeap)
 
 	for _, head := range lists {
 		if head != nil {
-			heap.Push(listHeap, head)
+			listHeap.Push(head)
 		}
 	}
 
 	current := dummy
 	for listHeap.Len() != 0 {
-		minHead := heap.Pop(listHeap).(*ListNode)
+		minHead := listHeap.Pop()
 		current.Next = minHead
 		current = current.Next
 
 		if minHead.Next != nil {
-			heap.Push(listHeap, minHead.Next)
+			listHeap.Push(minHead.Next)
 		}
 	}
 
