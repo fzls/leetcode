a4eaa24870e58693a4b2acec029859df4e3fc8ea
风之凌殇 <fzls.zju@gmail.com>
2019-09-23 00:15:51 +0800

    31 添加注释，同时参考题解，移除不必要的排序

--- 31.go
+++ 31.go
@@ -1,12 +1,15 @@
 package leetcode
 
-import "sort"
-
 // 2019/09/22 23:10 by fzls
 // 最小的排列是完全顺序的，最大的排列是完全逆序的，也就是说排列递增的过程其实是让逆序对增加的过程，所以可以考虑转化为如果使当前排列的逆序对增加最小值
+// 参考题解得到的理解：对于一个完全逆序的排列，我们无法找到比其更大的排列；所以我们从右往左找到第一个不是逆序的位置i,i+1（假设有）
+// 则 a[0],... a[i], a[i+1], ... a[n-1], 其中a[i]<a[i+1], a[i+2] >= a[i+3], ... a[n-2] >= a[n-1]
+// 然后我们从右往左，找到在i右侧第一个比i大的数，并将其与i交换位置，然后将i后方的全部翻转，则可以得到下一个排列
+// 画一个示意图可以很容易理解上面的做法
 func nextPermutation(nums []int) {
 	last := len(nums) - 1
-	for i := last - 1; i >= 0; i-- {
+	i := last - 1
+	for ; i >= 0; i-- {
 		// 找到第一个连续顺序队 i, i+1
 		if nums[i] < nums[i+1] {
 			// 从后往前找到第一个与该顺序对左元素构成顺序对的元素
@@ -17,13 +20,12 @@ func nextPermutation(nums []int) {
 			// 交换这组逆序对
 			nums[j], nums[i] = nums[i], nums[j]
 			// 将剩余部分排序，使该部分逆序数最小
-			sort.Ints(nums[i+1:])
-			return
+			break
 		}
 	}
 
 	// 完全逆序
-	i := 0
+	i++
 	j := last
 	for i < j {
 		nums[i], nums[j] = nums[j], nums[i]
