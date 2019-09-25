d9b310f245d5613fe0f6faaf84c6999823323463
风之凌殇 <fzls.zju@gmail.com>
2019-09-26 02:11:53 +0800

    697 优化一下计算方法

--- 697.go
+++ 697.go
@@ -1,40 +1,42 @@
 package leetcode
 
 // 2019/09/26 1:48 by fzls
+type _count struct {
+	cnt      int
+	firstIdx int
+	lastIdx  int
+}
+
+func (c *_count) Len() int {
+	return c.lastIdx - c.firstIdx + 1
+}
+
 func findShortestSubArray(nums []int) int {
 	if len(nums) == 0 {
 		return 0
 	}
 
-	counter := make(map[int]int)
-	mostNums := []int{nums[0]}
-	for _, num := range nums {
-		maxCnt := counter[mostNums[0]]
-
-		counter[num]++
-		cnt := counter[num]
-		if cnt > maxCnt {
-			mostNums = []int{num}
-		} else if cnt == maxCnt {
-			mostNums = append(mostNums, num)
+	counter := make(map[int]*_count)
+	for idx, num := range nums {
+		if cnt, ok := counter[num]; ok {
+			cnt.cnt++
+			cnt.lastIdx = idx
+		} else {
+			counter[num] = &_count{
+				cnt:      1,
+				firstIdx: idx,
+				lastIdx:  idx,
+			}
 		}
 	}
 
-	minLen := len(nums) + 1
-
-	for _, num := range mostNums {
-		i := 0
-		j := len(nums) - 1
-		for nums[i] != num {
-			i++
-		}
-		for nums[j] != num {
-			j--
-		}
-		if j-i+1 < minLen {
-			minLen = j - i + 1
+	var mc _count
+	for _, cnt := range counter {
+		if cnt.cnt > mc.cnt ||
+			cnt.cnt == mc.cnt && cnt.Len() < mc.Len() {
+			mc = *cnt
 		}
 	}
 
-	return minLen
+	return mc.Len()
 }
