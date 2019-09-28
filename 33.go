d0fb5efab14b2eac8367284d9f77a0a172396dcd
风之凌殇 <fzls.zju@gmail.com>
2019-09-29 00:51:33 +0800

    33 实现官方解法

--- 33.go
+++ 33.go
@@ -1,7 +1,12 @@
 package leetcode
 
 // 2019/09/28 23:03 by fzls
+
 func search(nums []int, target int) int {
+	return _search2(nums, target)
+}
+
+func _search(nums []int, target int) int {
 	if len(nums) == 0 {
 		return -1
 	}
@@ -70,4 +75,52 @@ func __bs(nums []int, low, high int, target int) int {
 		}
 	}
 	return -1
+}
+
+func _search2(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	if len(nums) == 1 {
+		if nums[0] == target {
+			return 0
+		} else {
+			return -1
+		}
+	}
+
+	changePoint := getChangePoint(nums)
+
+	if changePoint == -1 {
+		return __bs(nums, 0, len(nums)-1, target)
+	} else {
+		if target >= nums[0] {
+			return __bs(nums, 0, changePoint, target)
+		} else {
+			return __bs(nums, changePoint+1, len(nums)-1, target)
+		}
+	}
+}
+
+func getChangePoint(nums []int) int {
+	if nums[0] < nums[len(nums)-1] {
+		return -1
+	}
+
+	low := 0
+	high := len(nums) - 1
+	for low <= high {
+		i := int(uint(low+high) >> 1)
+		if nums[i] > nums[i+1] {
+			return i
+		} else {
+			if nums[i] >= nums[0] {
+				low = i + 1
+			} else {
+				high = i - 1
+			}
+		}
+	}
+
+	return 0
 }
