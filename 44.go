84e655a29fdc65459869a5db8d90dac242db80f3
风之凌殇 <fzls.zju@gmail.com>
2019-10-17 00:54:57 +0800

    44 参考dp方案

--- 44.go
+++ 44.go
@@ -1,9 +1,36 @@
 package leetcode
 
 // 2019/10/16 23:45 by fzls
-
 func __isMatch(s string, p string) bool {
+	return _matchDp(s, p)
 	return newIsMatchSolution(s, p).solve()
+}
+
+func _matchDp(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+	for j := 1; j <= n; j++ {
+		if p[j-1] == '*' {
+			dp[0][j] = dp[0][j-1]
+		}
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] == '?' || s[i-1] == p[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			}
+			if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-1] || dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[m][n]
 }
 
 type __isMatch_Key struct {
