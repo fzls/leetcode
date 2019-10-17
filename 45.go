f13892c62beff9029f9291104cc3f5bd1ca7a877
风之凌殇 <fzls.zju@gmail.com>
2019-10-18 00:05:25 +0800

    45

---
+++ 45.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import "math"
+
+// 2019/10/17 23:56 by fzls
+func jump(nums []int) int {
+	n := len(nums)
+	dp := make([]int, n)
+	dp[n-1] = 0
+
+	for i := n - 2; i >= 0; i-- {
+		ms := math.MaxInt32
+		for j := i + 1; j <= i+nums[i] && j < n; j++ {
+			if dp[j] < ms {
+				ms = dp[j]
+			}
+		}
+		dp[i] = 1 + ms
+	}
+
+	return dp[0]
+}
---
+++ 45_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func Test_jump(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		struct {
+			name string
+			args args
+			want int
+		}{name: "test", args: struct{ nums []int }{nums: []int{2, 3, 1, 1, 4}}, want: 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := jump(tt.args.nums); got != tt.want {
+				t.Errorf("jump() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
