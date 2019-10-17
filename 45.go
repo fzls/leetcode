f22dd9dcc10f9847596fe1e7b3e824b6311dc9f6
风之凌殇 <fzls.zju@gmail.com>
2019-10-18 00:14:10 +0800

    45 优化一下

--- 45.go
+++ 45.go
@@ -1,6 +1,8 @@
 package leetcode
 
-import "math"
+import (
+	"math"
+)
 
 // 2019/10/17 23:56 by fzls
 func jump(nums []int) int {
@@ -9,6 +11,11 @@ func jump(nums []int) int {
 	dp[n-1] = 0
 
 	for i := n - 2; i >= 0; i-- {
+		if i+nums[i] >= n-1 {
+			dp[i] = 1
+			continue
+		}
+
 		ms := math.MaxInt32
 		for j := i + 1; j <= i+nums[i] && j < n; j++ {
 			if dp[j] < ms {
