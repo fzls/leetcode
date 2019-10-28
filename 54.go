3d73296d0c572e3729631211a5e2f00ce4fa2917
风之凌殇 <fzls.zju@gmail.com>
2019-10-29 00:55:40 +0800

    54

--- 54.go
+++ 54.go
@@ -1,14 +1,55 @@
 package leetcode
 
-func lengthOfLastWord(s string) int {
-	i := len(s) - 1
-	for ; i >= 0 && s[i] == ' '; i-- {
+// 2019/10/29 0:16 by fzls
+func spiralOrder(matrix [][]int) []int {
+	if len(matrix) == 0 {
+		return nil
 	}
-	rStart := i
 
-	for ; i >= 0 && s[i] != ' '; i-- {
+	if len(matrix[0]) == 0 {
+		return nil
 	}
-	rEnd := i
 
-	return rStart - rEnd
+	m, n := len(matrix), len(matrix[0])
+	res := make([]int, 0, m*n)
+
+	startX, startY := 0, 0
+
+	step := 0
+	for {
+		endX, endY := startX+m-2*step-1, startY+n-2*step-1
+		if endX < startX || endY < startY {
+			break
+		}
+
+		// [x, y] -> [x, y + n-2*step -1]
+		for y := startY; y <= endY; y++ {
+			res = append(res, matrix[startX][y])
+		}
+
+		// [x+1, y+ n-2*step -1] -> [x + m-2*step-1, y+ n-2*step - 1]
+		for x := startX + 1; x <= endX; x++ {
+			res = append(res, matrix[x][endY])
+		}
+
+		// [x + m-2*step -1, y + n-2*step-1 -1] -> [x + m-2*step - 1, y]
+		if startX != endX {
+			for y := endY - 1; y >= startY; y-- {
+				res = append(res, matrix[endX][y])
+			}
+		}
+
+		// [x + m-2*step-1 -1, y] -> [x + 1, y]
+		if startY != endY {
+			for x := endX - 1; x >= startX+1; x-- {
+				res = append(res, matrix[x][startY])
+			}
+		}
+
+		step++
+		startX++
+		startY++
+	}
+
+	return res
 }
--- 54_test.go
+++ 54_test.go
@@ -1,22 +1,40 @@
 package leetcode
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func Test_lengthOfLastWord(t *testing.T) {
+// 2019/10/29 0:38 by fzls
+
+func Test_spiralOrder(t *testing.T) {
 	type args struct {
-		s string
+		matrix [][]int
 	}
 	tests := []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
-		{name: "test", args: struct{ s string }{s: "Hello World"}, want: 5},
+		{name: "test", args: struct{ matrix [][]int }{matrix: [][]int{
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+		}}, want: []int{
+			1, 2, 3, 6, 9, 8, 7, 4, 5,
+		}},
+		{name: "test", args: struct{ matrix [][]int }{matrix: [][]int{
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+		}}, want: []int{
+			1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7,
+		}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := lengthOfLastWord(tt.args.s); got != tt.want {
-				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			if got := spiralOrder(tt.args.matrix); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("spiralOrder(%v) = %v, want %v", tt.args, got, tt.want)
 			}
 		})
 	}
