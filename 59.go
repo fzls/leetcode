3b835bce4a421bbe456f2bb25250199770e7f37e
风之凌殇 <fzls.zju@gmail.com>
2019-11-01 01:29:48 +0800

    59

---
+++ 59.go
@@ -0,0 +1,55 @@
+package leetcode
+
+// 2019/11/01 1:09 by fzls
+func generateMatrix(n int) [][]int {
+	if n <= 0 {
+		return nil
+	}
+
+	// init res
+	buffer := make([]int, n*n)
+	res := make([][]int, n)
+	for i := 0; i < n; i++ {
+		res[i], buffer = buffer[:n], buffer[n:]
+	}
+
+	idx := 1
+	// assign value to res[row][col]
+	for step := 0; step <= (n-1)/2; step++ {
+		// find left top and right bottom
+		startRow, startCol := step, step
+		rowSize, colSize := n-2*step, n-2*step
+		endRow, endCol := startRow+rowSize-1, startCol+colSize-1
+
+		// left top[inclusive] to right top[inclusive]
+		for col := startCol; col <= endCol; col++ {
+			res[startRow][col] = idx
+			idx++
+		}
+
+		// right top[exclusive] to right bottom[inclusive]
+		for row := startRow + 1; row <= endRow; row++ {
+			res[row][endCol] = idx
+			idx++
+		}
+
+		// if rowSize > 1, right bottom[exclusive] to left bottom[inclusive]
+		if rowSize > 1 {
+			for col := endCol - 1; col >= startCol; col-- {
+				res[endRow][col] = idx
+				idx++
+			}
+		}
+
+		// if colSize > 1, left bottom[exclusive] to left top[exclusive]
+		if colSize > 1 {
+			for row := endRow - 1; row >= startRow+1; row-- {
+				res[row][startCol] = idx
+				idx++
+			}
+		}
+	}
+
+	// return
+	return res
+}
