1d80b64c6ee5ed7f65671438e1deb8fe34d33ee3
风之凌殇 <fzls.zju@gmail.com>
2019-11-02 00:53:48 +0800

    60 优化注释，增加可读性

--- 60.go
+++ 60.go
@@ -16,41 +16,56 @@ var fact = [...]int{
 
 // 2019/11/01 23:28 by fzls
 func getPermutation(n int, k int) string {
-	// check
+	checkParams(n, k)
+
+	nums := getInitalPerm(n)
+	batchPerm(k, nums)
+	return strPerm(nums)
+}
+
+func checkParams(n int, k int) {
 	if !(n >= 1 && n <= 9 && k >= 1 && k <= fact[n]) {
 		panic("invalid params")
 	}
+}
 
-	// init
+func getInitalPerm(n int) []int {
 	nums := make([]int, n)
 	for i := 0; i < n; i++ {
 		nums[i] = i + 1
 	}
+	return nums
+}
+
+// batch perm by step size of fact[i](1<=i<=n-1])
+func batchPerm(k int, nums []int) {
+	n := len(nums)
 
 	// find each number from left to right until k is 1
 	idx := 0
-	//fmt.Println(idx, k, nums)
 	for k != 1 {
-		rightPerm := fact[n-idx-1]
+		batchSize := fact[n-(idx+1)] // number of perms for idx+1,idx+2,...n-2,n-1
 
 		// find the first number by repeated reducing k by multiple of fact[n-1]
-		toPutLeftIdx := idx
-		for k > rightPerm {
-			toPutLeftIdx++
-			k -= rightPerm
+		targetIdx := idx
+		for k > batchSize {
+			targetIdx++
+			k -= batchSize
 		}
-		// now move first number to front and those before it to right
-		toPutLeft := nums[toPutLeftIdx]
-		for i := toPutLeftIdx; i > idx; i-- {
+		// now move target number to front and those before it to right
+		target := nums[targetIdx]
+		// copy idx, idx+1 ... targetIdx-1 to idx+1, idx+2 ... targetIdx
+		for i := targetIdx; i >= idx+1; i-- {
 			nums[i] = nums[i-1]
 		}
-		nums[idx] = toPutLeft
+		nums[idx] = target
 
 		idx++
-		//fmt.Println(idx, k, nums)
 	}
+}
 
-	// transform
+func strPerm(nums []int) string {
+	n := len(nums)
 	res := make([]byte, n)
 	for i := 0; i < n; i++ {
 		res[i] = '0' + byte(nums[i])
