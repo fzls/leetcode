c98f39c72cd3823a283352f79d37f39954457d75
风之凌殇 <fzls.zju@gmail.com>
2019-11-07 22:39:25 +0800

    73 额外空间复杂度由O(m*n)=>O(m+n)

--- 73.go
+++ 73.go
@@ -5,22 +5,38 @@ func setZeroes(matrix [][]int) {
 	m := len(matrix)
 	n := len(matrix[0])
 
-	copyed := make([][]int, m)
-	for i := 0; i < m; i++ {
-		copyed[i] = make([]int, n)
-		copy(copyed[i], matrix[i])
-	}
+	zeroRows := make([]bool, m)
+	zeroCols := make([]bool, n)
 
+	// find row and col that should zero out
 	for row := 0; row < m; row++ {
 		for col := 0; col < n; col++ {
-			if copyed[row][col] == 0 {
-				for i := 0; i < m; i++ {
-					matrix[i][col] = 0
-				}
-				for j := 0; j < n; j++ {
-					matrix[row][j] = 0
-				}
+			if matrix[row][col] == 0 {
+				zeroRows[row] = true
+				zeroCols[col] = true
 			}
+		}
+	}
+
+	// zero rows
+	for row := 0; row < m; row++ {
+		if !zeroRows[row] {
+			continue
+		}
+
+		for col := 0; col < n; col++ {
+			matrix[row][col] = 0
+		}
+	}
+
+	// zero cols
+	for col := 0; col < n; col++ {
+		if !zeroCols[col] {
+			continue
+		}
+
+		for row := 0; row < m; row++ {
+			matrix[row][col] = 0
 		}
 	}
 
